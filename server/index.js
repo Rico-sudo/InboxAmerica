e30a20b458c237d2cf498e17804484b7a0fbe591
Rico-sudo <albericosss@hotmail.it>
2021-01-07 15:14:22 +1100

    first commit

---
+++ server/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "popcorn",
+  database: "ppl_system",
+});
+
+app.post("/create", (req , res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const position = req.body.position;
+
+  db.query(
+    "INSERT INTO ppl_table (name ,age, position) VALUES (?,?,?)",
+    [name, age, position],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Done");
+      }
+    }
+  );
+});
+
+app.get("/persons" , (req , res)=>{
+    db.query("SELECT * FROM ppl_table " , (err, result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+app.put("/update", (req, res) => {
+    const id = req.body.id;
+    const position = req.body.position;
+    db.query(
+      "UPDATE ppl_table SET position = ? WHERE CAST(id as char(255)) = ?",
+      [position, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+  app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM ppl_table WHERE CAST(id as char(255)) = ?", id, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+  
+
+
+
+
+app.listen(3001, () => {
+  console.log("Server is running on 3001 :)");
+});
